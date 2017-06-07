a485e73fbaedc18fb2480163ca9e62d0b697ab63
Aradi Nóra <aradinora111@gmail.com>
2017-06-07 15:35:39 +0200

    add project and .gitignore

---
+++ src/main/java/com/chromediopside/GitinderApplication.java
@@ -0,0 +1,13 @@
+package com.chromediopside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitinderApplication.class, args);
+		System.out.println("Hello World!");
+	}
+}
