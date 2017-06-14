70a0b317f145f89694ca8833f411f5adccd6fb47
Patrik <horvath.patrik001@gmail.com>
2017-06-14 12:59:47 +0200

    Rename my_user to GiTinder_User table

--- src/main/java/com/chromediopside/model/GiTinderUser.java
+++ src/main/java/com/chromediopside/model/GiTinderUser.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class MyUser {
+public class GiTinderUser {
 
   @Id
   String username;
---
+++ src/main/resources/db/migration/V0_0_4__renametable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE GiTinder_user (
+  username VARCHAR(40) NOT NULL,
+  accessToken VARCHAR(100) NOT NULL,
+  appToken VARCHAR(16) NOT NULL,
+  PRIMARY KEY (username)
+);
