9a51b6e3e519dd84dedd24fbf41d8ad6d803a8a6
Patrik <horvath.patrik001@gmail.com>
2017-06-14 11:21:51 +0200

    solved bug

---
+++ src/main/resources/db/migration/V0_0_2__userinit.sql
@@ -0,0 +1,2 @@
+DROP TABLE my_users;
+DROP TABLE my_user;
--- src/main/resources/db/migration/V0_0_3__cretaemyuser.sql
+++ src/main/resources/db/migration/V0_0_3__cretaemyuser.sql
@@ -1,4 +1,4 @@
-CREATE TABLE My_users (
+CREATE TABLE My_user (
   username VARCHAR(40) NOT NULL,
   accessToken VARCHAR(100) NOT NULL,
   appToken VARCHAR(16) NOT NULL,
