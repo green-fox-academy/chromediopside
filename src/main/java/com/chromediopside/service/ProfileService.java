2771f084d4e252a55beeac2541fed7e5b024229a
Peter Czigany <p.czigany@gmail.com>
2017-06-20 15:27:23 +0200

    overwrite the method for getting github data
     - use authenticated GET request

--- src/main/java/com/chromediopside/service/ProfileService.java
+++ src/main/java/com/chromediopside/service/ProfileService.java
@@ -2,107 +2,55 @@ package com.chromediopside.service;
 
 import com.chromediopside.model.GiTinderProfile;
 import com.chromediopside.model.Language;
-import com.chromediopside.repository.LanguageRepository;
-import com.chromediopside.repository.ProfileRepository;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.eclipse.egit.github.core.service.UserService;
 
 public class ProfileService {
 
-  @Autowired
-  ProfileRepository profileRepository;
-  @Autowired
-  LanguageRepository languageRepository;
+  private static final String GET_REQUEST_IOERROR = "Some GitHub data not available for this accessToken!";
 
-  public GiTinderProfile fetchProfileByLogin(String login) {
-    return profileRepository.findByLogin(login);
-  }
-
-  public List<GiTinderProfile> randomTenProfileByLanguage(String languageName) {
-    Language selectingLanguage = languageRepository.findOne(languageName);
-    Set<GiTinderProfile> profilesSet = selectingLanguage.getProfileSet();
-    List<GiTinderProfile> profileList = new ArrayList<>();
-    profileList.addAll(profilesSet);
-
-    if (profileList.size() > 10) {
-    Collections.shuffle(profileList);
-    profileList.subList(0, 10);
-    }
-
-    return profileList;
-  }
-
-
-  public GiTinderProfile getGithubProfileData(String username) throws JSONException, IOException {
-    String githubDataApi = "https://api.github.com/users/" + username + "/repos";
-
-    JSONArray jsonArray = readJsonFromUrl(githubDataApi);
+  public GiTinderProfile getProfileFromGitHub(String accessToken) {
     GiTinderProfile giTinderProfile = new GiTinderProfile();
-    List<String> repos = new ArrayList<>();
-    List<Language> languageList = new ArrayList<>();
-    Set<String> stringSet = new HashSet<>();
-    Set<Language> languageSet = new HashSet<Language>(languageList);
 
-    giTinderProfile.setLogin(username);
-    giTinderProfile.setAvatarUrl(
-            jsonArray.getJSONObject(0).getJSONObject("owner").getString("avatar_url"));
+    GitHubClient gitHubClient = new GitHubClient();
+    gitHubClient.setOAuth2Token(accessToken);
 
-    for (int i = 0; i < jsonArray.length(); i++) {
-      repos.add(jsonArray.getJSONObject(i).getString("name"));
-
-      String language = jsonArray.getJSONObject(i).getString("language");
-      languageList.add(new Language(language));
-
-      for (Language languageElement : languageList) {
-        stringSet.add(languageElement.getLanguageName());
-      }
-
-      for (String string : stringSet) {
-        languageList.clear();
-        languageList.add(new Language(string));
-        languageSet.addAll(languageList);
-      }
-    }
-    giTinderProfile.setRepos(String.join("; ", repos));
-    giTinderProfile.setLanguagesList(languageSet);
-
-    return giTinderProfile;
-  }
-
-
-  private static String readAll(Reader reader) throws IOException {
-    StringBuilder stringBuilder = new StringBuilder();
-    int cp;
-    while ((cp = reader.read()) != -1) {
-      stringBuilder.append((char) cp);
-    }
-    return stringBuilder.toString();
-  }
-
-
-  private static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
-    InputStream inputStream = new URL(url).openStream();
+    UserService userService = new UserService(gitHubClient);
+    RepositoryService repositoryService = new RepositoryService(gitHubClient);
     try {
-      BufferedReader bufferedReader = new BufferedReader(
-              new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-      String jsonText = readAll(bufferedReader);
-      JSONArray json = new JSONArray(jsonText);
-      return json;
-    } finally {
-      inputStream.close();
+      User user = userService.getUser();
+      giTinderProfile.setLogin(user.getLogin());
+      giTinderProfile.setAvatarUrl(user.getAvatarUrl());
+      List<Repository> repositoryList = repositoryService.getRepositories();
+      List<String> repos = new ArrayList<>();
+      List<String> languages = new ArrayList<>();
+      for (Repository currentRepo :
+          repositoryList) {
+        repos.add(currentRepo.getName());
+        String repoLanguage = currentRepo.getLanguage();
+        if (!languages.contains(repoLanguage)) {
+          languages.add(repoLanguage);
+        }
+      }
+      giTinderProfile.setRepos(String.join(";", repos));
+      Set<Language> languageObjects = new HashSet<>();
+      for (String currentLanguage :
+          languages) {
+        languageObjects.add(new Language(currentLanguage));
+      }
+      giTinderProfile.setLanguagesList(languageObjects);
+      return giTinderProfile;
+    } catch (IOException e) {
+      System.out.println(GET_REQUEST_IOERROR);
+      return null;
     }
   }
 
