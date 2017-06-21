cc482ffddedf233dcc7179ee978836b1b8707d39
Patrik <horvath.patrik001@gmail.com>
2017-06-21 18:14:32 +0200

    create mock user test

--- src/test/java/com/chromediopside/service/UserServiceTest.java
+++ src/test/java/com/chromediopside/service/UserServiceTest.java
@@ -2,27 +2,35 @@ package com.chromediopside.service;
 
 import static org.junit.Assert.*;
 
-import com.chromediopside.model.GiTinderUser;
+import com.chromediopside.mockbuilder.MockUserBuilder;
 import com.chromediopside.repository.UserRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserServiceTest {
 
+  @Autowired
+  private GiTinderUserService userService;
+  @Autowired
+  private MockUserBuilder mockUserBuilder;
+
   @Test
   public void getUserObjectByAppTokenTest() {
-    GiTinderUser expectedGiTinderUser = new GiTinderUser("testa", "a23456789101112a", "aa345678910111aa");
-
     UserRepository mockUserRepo = Mockito.mock(UserRepository.class);
-    Mockito.when(mockUserRepo.findByAppToken("aa345678910111aa")).thenReturn(expectedGiTinderUser);
-
-    GiTinderUserService userService = new GiTinderUserService(mockUserRepo);
+    Mockito.when(mockUserRepo.findByAppToken("aa345678910111aa")).thenReturn(mockUserBuilder.build());
+    userService.setUserRepo(mockUserRepo);
     Object actualGiTinderUser = userService.getUserObjectByAppToken("aa345678910111aa");
 
-    assertEquals(expectedGiTinderUser.toString(), actualGiTinderUser.toString());
+    assertEquals(mockUserBuilder.build(), actualGiTinderUser);
   }
 }
