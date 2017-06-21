cac8060948bb130eab48459b37437a79c781b70e
Patrik <horvath.patrik001@gmail.com>
2017-06-21 15:45:12 +0200

    unittest upload

---
+++ src/test/java/com/chromediopside/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.chromediopside.service;
+
+import static org.junit.Assert.*;
+
+import com.chromediopside.model.GiTinderUser;
+import com.chromediopside.repository.UserRepository;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserServiceTest {
+
+  @Test
+  public void getUserObjectByAppTokenTest() {
+    GiTinderUser expectedGiTinderUser = new GiTinderUser("testa", "a23456789101112a", "aa345678910111aa");
+
+    UserRepository mockUserRepo = Mockito.mock(UserRepository.class);
+    Mockito.when(mockUserRepo.findByAppToken("aa345678910111aa")).thenReturn(expectedGiTinderUser);
+
+    UserService userService = new UserService(mockUserRepo);
+    Object actualGiTinderUser = userService.getUserObjectByAppToken("aa345678910111aa");
+
+    assertEquals(expectedGiTinderUser.toString(), actualGiTinderUser.toString());
+  }
+}
