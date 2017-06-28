79892c6cd81cabdfb30a9b22d78552c0692852c6
Daniel Szalay <sza.dan.uj@gmail.com>
2017-06-28 14:25:41 +0200

    add public static final error message for getrequest IOError

--- src/main/java/com/chromediopside/service/GitHubClientSevice.java
+++ src/main/java/com/chromediopside/service/GitHubClientSevice.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class GitHubClientSevice {
 
+  private static final String GET_REQUEST_IOERROR
+      = "Some GitHub data of this user is not available for this token!";
+
+  public static String getGetRequestIoerror() {
+    return GET_REQUEST_IOERROR;
+  }
+
   public static GitHubClient setUpGitHubClient(String accessToken) {
     GitHubClient gitHubClient = new GitHubClient();
     gitHubClient.setOAuth2Token(accessToken);
