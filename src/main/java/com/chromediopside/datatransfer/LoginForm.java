cdc41c2fafc96c4e1a5979945ccfacf5f99ce686
Peter Czigany <p.czigany@gmail.com>
2017-06-29 12:01:10 +0200

    Fix service method logic

--- src/main/java/com/chromediopside/datatransfer/LoginForm.java
+++ src/main/java/com/chromediopside/datatransfer/LoginForm.java
@@ -1,5 +1,7 @@
 package com.chromediopside.datatransfer;
 
+import com.chromediopside.model.GiTinderProfile;
+import java.util.Objects;
 import javax.validation.constraints.NotNull;
 
 public class LoginForm {
@@ -26,5 +28,15 @@ public class LoginForm {
 
   public void setAccessToken(String accessToken) {
     this.accessToken = accessToken;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof LoginForm)) {
+      return false;
+    }
+    LoginForm form = (LoginForm) o;
+    return Objects.equals(username, form.username) &&
+        Objects.equals(accessToken, form.accessToken);
   }
 }
