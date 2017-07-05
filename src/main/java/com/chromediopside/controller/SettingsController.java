fb94f6d483aefba16f6b5b2f4cff588f415de52c
Patrik <horvath.patrik001@gmail.com>
2017-07-05 10:22:49 +0200

    create Settings Controller Test Class - initial commit

--- src/main/java/com/chromediopside/controller/SettingsController.java
+++ src/main/java/com/chromediopside/controller/SettingsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class SettingsController {
     this.userService = userService;
     this.errorService = errorService;
     this.settingsService = settingsService;
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> exception(Exception ex) {
+    return new ResponseEntity<>(errorService.unauthorizedRequestError(), HttpStatus.UNAUTHORIZED);
   }
 
   @CrossOrigin("*")
