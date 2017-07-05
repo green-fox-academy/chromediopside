f10ee4a336c0e616417373971f7713ef486d86f9
Patrik <horvath.patrik001@gmail.com>
2017-07-05 10:23:47 +0200

    add getSettings Without Token Test

--- src/test/java/com/chromediopside/controller/SettingsControllerTest.java
+++ src/test/java/com/chromediopside/controller/SettingsControllerTest.java
@@ -1,6 +1,9 @@
 package com.chromediopside.controller;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import com.chromediopside.GitinderApplication;
@@ -8,11 +11,13 @@ import com.chromediopside.repository.SettingRepository;
 import com.chromediopside.repository.UserRepository;
 import com.chromediopside.service.ProfileService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -41,6 +46,18 @@ public class SettingsControllerTest {
   @Before
   public void setup() throws Exception {
     this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void getSettingsWithoutToken() throws Exception {
+
+    this.mockMvc.perform(get("/settings"))
+            .andExpect(status().isUnauthorized())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(content().json("{"
+                    + "\"status\" : \"error\","
+                    + "\"message\" : \"Unauthorized request!\""
+                    + "}"));
   }
 
 }
