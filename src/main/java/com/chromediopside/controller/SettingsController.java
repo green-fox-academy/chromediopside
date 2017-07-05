e029cbd9660311b4a6e92697297152f8cd22ceb1
Patrik <horvath.patrik001@gmail.com>
2017-07-05 09:48:21 +0200

    create SettingsController and /settings end point

---
+++ src/main/java/com/chromediopside/controller/SettingsController.java
@@ -0,0 +1,38 @@
+package com.chromediopside.controller;
+
+import com.chromediopside.model.GiTinderUser;
+import com.chromediopside.service.ErrorService;
+import com.chromediopside.service.GiTinderUserService;
+import com.chromediopside.service.SettingsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SettingsController {
+
+  private GiTinderUserService userService;
+  private ErrorService errorService;
+  private SettingsService settingsService;
+
+  @Autowired
+  public SettingsController(GiTinderUserService userService, ErrorService errorService) {
+    this.userService = userService;
+    this.errorService = errorService;
+  }
+
+  @CrossOrigin("*")
+  @GetMapping("/settings")
+  public ResponseEntity<Object> getUserSettings(
+          @RequestHeader(name = "X-GiTinder-token") String appToken) {
+    if (!userService.validAppToken(appToken)) {
+      return new ResponseEntity<>(errorService.unauthorizedRequestError(), HttpStatus.UNAUTHORIZED);
+    }
+    return new ResponseEntity<>(settingsService.getUserSettings(appToken), HttpStatus.OK);
+  }
+
+}
