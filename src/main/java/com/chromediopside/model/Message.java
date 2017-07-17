ff27d4678c3e30cd6aa0bc3dba65317bf7b7d576
Aradi Nóra <aradinora111@gmail.com>
2017-07-17 16:15:10 +0200

    create message model

---
+++ src/main/java/com/chromediopside/model/Message.java
@@ -0,0 +1,37 @@
+package com.chromediopside.model;
+
+import java.sql.Timestamp;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+  private long id;
+  private String from;
+  private String to;
+  private Timestamp createdAt;
+  private String message;
+
+  public Message() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
