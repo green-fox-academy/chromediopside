4b405d09e9e43a60206db767ea0903a8a95fc366
Daniel Szalay <sza.dan.uj@gmail.com>
2017-07-18 09:29:38 +0200

    add table names

--- src/main/java/com/chromediopside/model/Message.java
+++ src/main/java/com/chromediopside/model/Message.java
@@ -1,17 +1,28 @@
 package com.chromediopside.model;
 
 import java.sql.Timestamp;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "message")
 public class Message {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "message_id")
   private long id;
+  @Column(name = "message_sender")
   private String from;
+  @Column(name = "recipient")
   private String to;
+  @Column(name = "created_at")
   private Timestamp createdAt;
+  @Column(name = "message_text")
   private String message;
 
   public Message() {
@@ -21,19 +32,39 @@ public class Message {
     return id;
   }
 
+  public void setId(long id) {
+    this.id = id;
+  }
+
   public String getFrom() {
     return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
   }
 
   public String getTo() {
     return to;
   }
 
+  public void setTo(String to) {
+    this.to = to;
+  }
+
   public Timestamp getCreatedAt() {
     return createdAt;
   }
 
+  public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+  }
+
   public String getMessage() {
     return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
   }
 }
